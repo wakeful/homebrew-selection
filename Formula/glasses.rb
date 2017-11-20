608d7dd95bc60e6998cf9f42887525961814061c
AJ <aj@48k.io>
2017-11-20 19:37:38 +0000

    bump `glasses` version

--- Formula/glasses.rb
+++ Formula/glasses.rb
@@ -1,9 +1,9 @@
 class Glasses < Formula
   desc "populate your local /etc/hosts with k8s Ingress Resources."
   homepage "https://github.com/wakeful/glasses"
-  version "0.1.0"
+  version "0.2.0"
   url "https://github.com/wakeful/glasses/releases/download/#{version}/glasses-darwin-amd64"
-  sha256 "b1079e0511d8fcbc99ed71225c5dbf88f6712ac8795b8622781f4d8948617fa9"
+  sha256 "7cee3632f2dca1b249bd513ef89734ac662835055928b445d8b6f78cb6367157"
 
   def install
     bin.install "glasses-darwin-amd64" => "glasses"
