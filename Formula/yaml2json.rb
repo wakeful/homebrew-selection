87f7f3099c066cbeb2588431f9093b501c6726d8
AJ <aj@48k.io>
2018-10-30 20:19:19 +0000

    correct yaml2json sha256 sum

--- Formula/yaml2json.rb
+++ Formula/yaml2json.rb
@@ -3,7 +3,7 @@ class Yaml2json < Formula
   homepage "https://github.com/wakeful/yaml2json"
   version "0.3.1"
   url "https://github.com/wakeful/yaml2json/releases/download/#{version}/yaml2json-darwin-amd64"
-  sha256 "54dac603087827033e74f9396e220e70a463a5949bfc36ce0e2c445ab41845cb"
+  sha256 "466763d034da7014c8cb4b8208ef0cf750a76a90974f8ddaa717023e413ffa51"
 
   def install
     bin.install "yaml2json-darwin-amd64" => "yaml2json"
