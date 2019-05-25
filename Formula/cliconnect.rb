8eb23de863de1a92018bb236f1aacafd9fb45067
AJ <aj@48k.io>
2019-05-25 15:22:52 +0200

    bump cliconnect version

--- Formula/cliconnect.rb
+++ Formula/cliconnect.rb
@@ -1,9 +1,9 @@
 class Cliconnect < Formula
   desc "a small cli tool to work with kafka connect."
   homepage "https://github.com/wakeful/cliconnect"
-  version "0.3.0"
-  url "https://github.com/wakeful/cliconnect/releases/download/0.2.0/cliconnect-darwin-amd64"
-  sha256 "736dd6c9ee91e84fb4cefcf896d0a50b2ba718892f0d95da0c7789183804db5a"
+  version "0.3.1"
+  url "https://github.com/wakeful/cliconnect/releases/download/#{version}/cliconnect-darwin-amd64"
+  sha256 "2fd99577f828446511c5724a643944d4f514ee493c261438028b3ba5c2937211"
 
   def install
     bin.install "cliconnect-darwin-amd64" => "cliconnect"
