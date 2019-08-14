e3506189dcb2d23d213ad5f6ab69135030a61e03
AJ <aj@48k.io>
2019-08-14 10:30:07 +0100

    bump `yaml2json` version

--- Formula/yaml2json.rb
+++ Formula/yaml2json.rb
@@ -1,9 +1,9 @@
 class Yaml2json < Formula
   desc "convert YAML to JSON."
   homepage "https://github.com/wakeful/yaml2json"
-  version "0.3.2"
+  version "0.4.0"
   url "https://github.com/wakeful/yaml2json/releases/download/#{version}/yaml2json-darwin-amd64"
-  sha256 "8fd44356b00685deb5f9194b72a8a7d0ee033f83515950473b308a0fdb089a5e"
+  sha256 "c31431f0b52d16d592c6f8c563655da54221f718ccabd32428ca0be1f265c419"
 
   def install
     bin.install "yaml2json-darwin-amd64" => "yaml2json"
